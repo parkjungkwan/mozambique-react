17b513e5b03e9b76a79a23b3397782f764871205
parkjungkwan <pakjkwan@gmail.com>
2022-12-02 11:25:13 +0900

    11

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Counter from "./components/Counter"
+import TodoInput from "./components/todos/TodoInput"
 
 const App = () => {
   return (<>
   <h1> 리액트 홈 </h1>
   <Routes>
   <Route path="/" element={<Counter/>}></Route>
+  <Route path="/todos" element={<TodoInput/>}></Route>
   </Routes></>)
 }
 export default App
---
+++ components/todos/TodoInput.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+const TodoInput = () => {
+    const [todo, setTodo] = useState('')
+    return (<>
+    <h3>할일 등록</h3>
+    </>)
+}
+export default TodoInput
