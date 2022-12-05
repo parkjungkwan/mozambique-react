2481f14c1e9c7426a0b489efb4688a4d545525ff
parkjungkwan <pakjkwan@gmail.com>
2022-12-05 16:24:05 +0900

    16

--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -1,19 +1,24 @@
 import { useDispatch, useSelector } from 'react-redux'
+import './TodoList.css'
 import {} from '../../store'
 
 const TodoList = () => {
     return (<>
     <h2>할일 목록</h2>
-    <table>
-    <tr>
-        <th>Todo List</th>   
-    </tr>
-    <tr>
-        <td>Alfreds Futterkiste</td>  
-    </tr>
-    <tr>
-        <td>Centro comercial Moctezuma</td>   
-    </tr>  
+    <table className='table'>
+    <thead>
+        <tr>
+            <th>Todo List</th>   
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Alfreds Futterkiste</td>  
+        </tr>
+        <tr>
+            <td>Centro comercial Moctezuma</td>   
+        </tr>  
+    </tbody>
     </table>
     </>)
 }
