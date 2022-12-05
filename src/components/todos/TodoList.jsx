332d11bd2500d9aca29ad4dc00336e3278d8747e
parkjungkwan <pakjkwan@gmail.com>
2022-12-05 16:35:45 +0900

    16

--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -1,8 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux'
 import './TodoList.css'
 import {} from '../../store'
+import { deleteTodoAction, toggleTodoAction } from '../../store/todo.reducer'
 
 const TodoList = () => {
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+    const toggleTodo = id => dispatch(toggleTodoAction(id))
+    const deleteTodo = id => dispatch(deleteTodoAction(id))
     return (<>
     <h2>할일 목록</h2>
     <table className='table'>
@@ -12,12 +17,12 @@ const TodoList = () => {
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>Alfreds Futterkiste</td>  
-        </tr>
-        <tr>
-            <td>Centro comercial Moctezuma</td>   
-        </tr>  
+        {todos.length === 0 && (<tr>
+            <td>등록된 할 일이 없습니다</td>  
+        </tr>)}
+        {todos.length !== 0 && (<tr>
+            <td>등록된 할 일이 있습니다</td>   
+        </tr>)} 
     </tbody>
     </table>
     </>)
