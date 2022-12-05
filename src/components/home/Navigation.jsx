8edd936a914001c15c8f5f5d84f1ed12e27a600b
parkjungkwan <pakjkwan@gmail.com>
2022-12-05 17:42:53 +0900

    final

--- src/components/home/Navigation.jsx
+++ src/components/home/Navigation.jsx
@@ -5,6 +5,8 @@ const Navigation = () => <nav>
         <li><Link to="/home">Home</Link></li>
         <li><Link to="/counter">Counter</Link></li>
         <li><Link to="/todos">Todos</Link></li>
+        <li><Link to="/signup">Sign UP</Link></li>
+        <li><Link to="/login">Login</Link></li>
     </ul>
 </nav>
 
